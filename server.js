57fc178fb85c7c23e8b988bbf77346d7692b9c36
supun19 <supun.12@cse.mrt.ac.lk>
2018-08-27 12:07:49 +0530

    json end point

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/',function (req,res) {
-    res.sendfile('index.html');
+    res.send(JSON.stringify([
+        {
+            "id": "1005@158.85.187.61",
+            "name": "1234",
+            "type": "videosipgw"
+        },
+        {
+            "id": "address2@sip.domain.com",
+            "name": "Some room name2",
+            "type": "videosipgw"
+        }
+    ]));
 })
 app.listen( 3000,function () {
     console.log("ServerListen Port:3000....")
