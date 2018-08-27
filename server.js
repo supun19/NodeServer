37735917a772984e83092b746606d8775073e277
supun19 <supun.12@cse.mrt.ac.lk>
2018-08-27 12:18:56 +0530

    json end point

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/',function (req,res) {
+app.get('/*',function (req,res) {
     res.send(JSON.stringify([
         {
             "id": "1005@158.85.187.61",
