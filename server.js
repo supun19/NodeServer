5246807ca9d654a8b79e3ba2b15638728c047d40
supun19 <supun.12@cse.mrt.ac.lk>
2018-09-20 15:13:05 +0530

    confernce reservation

--- server.js
+++ server.js
@@ -5,19 +5,14 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.get('/*',function (req,res) {
-    res.send(JSON.stringify([
-        {
-            "id": "1005@158.85.187.61",
-            "name": "1234",
-            "type": "videosipgw"
-        },
-        {
-            "id": "address2@sip.domain.com",
-            "name": "Some room name2",
-            "type": "videosipgw"
-        }
-    ]));
+app.get('/conference',function (req,res) {
+    res.send(JSON.stringify({
+        'id': 364758328,
+        'name': '1234',
+        'mail_owner': 'nipuna@moderator.jitsimeet.meetrix.io',
+        'start_time': '2018-09-20T09:10:40.421Z',
+        'duration': 900000
+    }));
 })
 app.listen( 3000,function () {
     console.log("ServerListen Port:3000....")
