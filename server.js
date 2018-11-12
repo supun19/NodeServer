e3479ee079cdb59b303877ab324853c7ad3addbd
supun19 <supun.12@cse.mrt.ac.lk>
2018-11-12 18:07:33 +0530

    barcode reader

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var fs = require('fs');
 var bodyParser = require('body-parser');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('./key.pem', 'utf8');
-var certificate = fs.readFileSync('./cert.pem', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(express.static("public"));
+app.use(express.static('public'))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -20,8 +13,6 @@ app.use(function(req, res, next) {
 app.get('/',function (req,res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(8443);
+app.listen( 3000,function () {
+    console.log("ServerListen Port:3000....")
+})
