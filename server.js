9d6b2926e30d541dca50627fdfee92ebb68e4781
supun19 <supun.12@cse.mrt.ac.lk>
2018-11-12 17:46:12 +0530

    barcode reader

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+var fs = require('fs');
 var bodyParser = require('body-parser');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('./key.pem', 'utf8');
+var certificate = fs.readFileSync('./cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+app.use(express.static("public"));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.post('/conference',function (req,res) {
-    console.log("confernce",req.query)
-    res.send(JSON.stringify({
-        'id': 364758328,
-        'name': '1234',
-        'mail_owner': 'nipuna@moderator.jitsimeet.meetrix.io',
-        'start_time': '2018-12-20T09:10:40.421Z',
-        'duration': 900000
-    }));
+app.get('/',function (req,res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
-app.listen( 3000,function () {
-    console.log("ServerListen Port:3000....")
-})
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
