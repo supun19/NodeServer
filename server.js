e3ff68eb2a4dd671bf3686d85d82822438e8ea1b
supun19 <supun.12@cse.mrt.ac.lk>
2019-08-08 10:04:32 +0530

    render html page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -18,6 +19,9 @@ app.post('/conference',function (req,res) {
         'start_time': '2018-12-20T09:10:40.421Z',
         'duration': 900000
     }));
+})
+app.get('/',function (req,res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.listen( 3000,function () {
     console.log("ServerListen Port:3000....")
