9925413772961eab8b21cc2ac463a6a5b16ad969
supun19 <supun.12@cse.mrt.ac.lk>
2020-04-29 18:03:41 +0530

    reservation api

--- server.js
+++ server.js
@@ -5,24 +5,35 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.get('/conference',function (req,res) {
-    console.log("confernce",req.query)
-    res.send(JSON.stringify({
-        'id': 364758328,
-        'name': '1234',
-        'mail_owner': 'nipuna@moderator.jitsimeet.meetrix.io',
-        'start_time': '2018-12-20T09:10:40.421Z',
-        'duration': 900000
-    }));
+app.post('/conference', function (req, res) {
+    const validUser = true
+    if (validUser) {
+        res.status(200);
+        res.send(JSON.stringify({
+            'id': 364758328,
+            'name': '1234',
+            'mail_owner': 'nipuna@moderator.jitsimeet.meetrix.io',
+            'start_time': '2020-04-28T09:10:40.421Z',
+            'duration': 90000000
+        }));
+    } else {
+        res.status(409);
+        res.send(JSON.stringify({
+            'conflict_id': 364758328
+        }));
+    }
+
 })
-app.get('/',function (req,res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.delete('/conference/:id', function (req, res) {
+    res.status(200);
+    res.send().end();
+
 })
-app.listen( 3000,function () {
+app.listen(3000, function () {
     console.log("ServerListen Port:3000....")
 })
